7a4a659be966df27bde179431edf2dffb540fde8
zhihuizhang <1152947493@qq.com>
2025-01-23 11:18:42 +0800

    format

--- web/simpletools/src/App.vue
+++ web/simpletools/src/App.vue
@@ -26,8 +26,8 @@ const loadFromLocalStorage = () => {
   const savedLayout = localStorage.getItem('currentLayout');
 
   if (savedTools) {
-  const parsedTools = JSON.parse(savedTools);
-  tools.value = parsedTools.map(tool => {
+    const parsedTools = JSON.parse(savedTools);
+    tools.value = parsedTools.map(tool => {
       return {
         ...tool,
         component: defineAsyncComponent({
@@ -110,8 +110,8 @@ const getToolStyle = (index) => {
           index === 0
             ? '1 / 1 / 2 / 2'
             : index === 1
-            ? '1 / 2 / 2 / 3'
-            : '2 / 1 / 2 / 3',
+              ? '1 / 2 / 2 / 3'
+              : '2 / 1 / 2 / 3',
       };
     } else if (layout === 'lyt2' || layout === 'lyt6') {
       // 上下分布 下方两格，上方一格
@@ -120,8 +120,8 @@ const getToolStyle = (index) => {
           index === 0
             ? '1 / 1 / 2 / 3'
             : index === 1
-            ? '2 / 1 / 3 / 2'
-            : '2 / 2 / 3 / 3',
+              ? '2 / 1 / 3 / 2'
+              : '2 / 2 / 3 / 3',
       };
     } else if (layout === 'lyt3' || layout === 'lyt7') {
       // 左右分布，左侧两格，右侧一格
@@ -130,8 +130,8 @@ const getToolStyle = (index) => {
           index === 0
             ? '1 / 1 / 2 / 2'
             : index === 1
-            ? '2 / 1 / 3 / 2'
-            : '1 / 2 / 3 / 3',
+              ? '2 / 1 / 3 / 2'
+              : '1 / 2 / 3 / 3',
       };
     } else if (layout === 'lyt4' || layout === 'lyt8') {
       // 左右分布，右侧两格，左侧一格
@@ -140,8 +140,8 @@ const getToolStyle = (index) => {
           index === 0
             ? '1 / 1 / 3 / 1'
             : index === 1
-            ? '1 / 2 / 2 / 3'
-            : '2 / 2 / 3 / 3',
+              ? '1 / 2 / 2 / 3'
+              : '2 / 2 / 3 / 3',
       };
     }
   } else if (toolCount === 4) {
@@ -151,10 +151,10 @@ const getToolStyle = (index) => {
         index === 0
           ? '1 / 1 / 2 / 2'
           : index === 1
-          ? '1 / 2 / 2 / 3'
-          : index === 2
-          ? '2 / 1 / 2 / 2'
-          : '2 / 2 / 2 / 3',
+            ? '1 / 2 / 2 / 3'
+            : index === 2
+              ? '2 / 1 / 2 / 2'
+              : '2 / 2 / 2 / 3',
     };
   }
 };
@@ -230,7 +230,9 @@ watch(isToolMarketVisible, (newVal) => {
     <!-- 加号和提示文本 -->
     <div v-if="tools.length === 0" class="center-content">
       <div class="add-button" @click="isToolMarketVisible = true">
-        <el-icon><Plus /></el-icon>
+        <el-icon>
+          <Plus />
+        </el-icon>
       </div>
       <div class="tip-text">
         当前操作面板无工具，请前往工具市场添加工具
@@ -238,22 +240,15 @@ watch(isToolMarketVisible, (newVal) => {
     </div>
 
     <!-- 添加的工具组件 -->
-  <div v-else class="tool-container" :class="currentLayout">
-      <div
-        v-for="(tool, index) in tools"
-        :key="index"
-        class="tool-item"
-        :style="getToolStyle(index)"
-        :class="{ 'drag-over': dragOverIndex === index }"
-        draggable="true"
-        @dragstart="onDragStart(index)"
-        @dragover="onDragOver($event, index)"
-        @drop="onDrop(index)"
-        @dragend="onDragEnd"
-      >
+    <div v-else class="tool-container" :class="currentLayout">
+      <div v-for="(tool, index) in tools" :key="index" class="tool-item" :style="getToolStyle(index)"
+        :class="{ 'drag-over': dragOverIndex === index }" draggable="true" @dragstart="onDragStart(index)"
+        @dragover="onDragOver($event, index)" @drop="onDrop(index)" @dragend="onDragEnd">
         <!-- 关闭按钮 -->
         <div class="close-button" @click="removeTool(index)">
-          <el-icon><Close /></el-icon>
+          <el-icon>
+            <Close />
+          </el-icon>
         </div>
         <!-- 动态渲染组件 -->
         <component :is="tool.component" />
@@ -265,46 +260,43 @@ watch(isToolMarketVisible, (newVal) => {
   <div class="button-group">
     <el-tooltip content="系统消息" placement="left">
       <el-button class="action-button">
-        <el-icon><Message /></el-icon>
+        <el-icon>
+          <Message />
+        </el-icon>
       </el-button>
     </el-tooltip>
 
     <el-tooltip content="联系作者1152947493@qq.com" placement="left">
       <el-button class="action-button">
-        <el-icon><ChatLineRound /></el-icon>
+        <el-icon>
+          <ChatLineRound />
+        </el-icon>
       </el-button>
     </el-tooltip>
 
     <el-tooltip content="工具市场" placement="left">
       <el-button class="action-button" @click="isToolMarketVisible = true">
-        <el-icon><Shop /></el-icon>
+        <el-icon>
+          <Shop />
+        </el-icon>
       </el-button>
     </el-tooltip>
 
     <el-tooltip content="网格布局" placement="left">
       <el-button class="action-button" @click="isLayoutModalVisible = true">
-        <el-icon><Grid /></el-icon>
+        <el-icon>
+          <Grid />
+        </el-icon>
       </el-button>
     </el-tooltip>
   </div>
 
   <!-- 工具市场模态框 -->
-  <el-dialog
-    v-model="isToolMarketVisible"
-    :width="'80%'"
-    :style="{ height: '70vh' }"
-    class="fixed-dialog"
-  >
+  <el-dialog v-model="isToolMarketVisible" :width="'80%'" :style="{ height: '70vh' }" class="fixed-dialog">
     <!-- 自定义标题区域 -->
     <template #header>
       <div class="dialog-header">
-        <el-input
-          v-model="searchQuery"
-          placeholder="按名字或类型搜索工具"
-          clearable
-          class="search-input"
-          suffix-icon="Search"
-        />
+        <el-input v-model="searchQuery" placeholder="按名字或类型搜索工具" clearable class="search-input" suffix-icon="Search" />
       </div>
     </template>
 
@@ -313,19 +305,16 @@ watch(isToolMarketVisible, (newVal) => {
   </el-dialog>
 
   <!-- 布局模态框 -->
-  <el-dialog
-    v-model="isLayoutModalVisible"
-    title="选择布局方式"
-    :width="'60%'"
-    :style="{ height: '70vh' }"
-    class="fixed-dialog"
-  >
+  <el-dialog v-model="isLayoutModalVisible" title="选择布局方式" :width="'60%'" :style="{ height: '70vh' }"
+    class="fixed-dialog">
     <LayoutModal :current-layout="currentLayout" @change-layout="changeLayout" />
   </el-dialog>
 </template>
 
 <style>
-html, body, #app {
+html,
+body,
+#app {
   margin: 0;
   padding: 0;
 }
@@ -342,15 +331,19 @@ html, body, #app {
   overflow: hidden;
   position: relative;
   display: flex;
-  justify-content: center; /* 水平居中 */
-  align-items: center; /* 垂直居中 */
+  justify-content: center;
+  /* 水平居中 */
+  align-items: center;
+  /* 垂直居中 */
 }
 
 .center-content {
   display: flex;
   flex-direction: column;
-  align-items: center; /* 水平居中 */
-  gap: 0.5rem; /* 加号和文本之间的间距 */
+  align-items: center;
+  /* 水平居中 */
+  gap: 0.5rem;
+  /* 加号和文本之间的间距 */
 }
 
 .add-button {
@@ -384,20 +377,27 @@ html, body, #app {
   justify-content: center;
   align-items: center;
   background-color: #f5f5f5;
-  position: relative; /* 相对定位，用于关闭按钮的绝对定位 */
+  position: relative;
+  /* 相对定位，用于关闭按钮的绝对定位 */
 }
 
 .tool-item:hover {
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* 添加阴影 */
-  transform: scale(1.001); /* 轻微放大 */
-  transition: transform 0.3s ease; /* 平滑过渡 */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  /* 添加阴影 */
+  transform: scale(1.001);
+  /* 轻微放大 */
+  transition: transform 0.3s ease;
+  /* 平滑过渡 */
 }
 
 /* 拖拽预览样式 */
 .tool-item.drag-over {
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* 添加阴影 */
-  transform: scale(1.001); /* 轻微放大 */
-  transition: transform 0.3s ease; /* 平滑过渡 */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  /* 添加阴影 */
+  transform: scale(1.001);
+  /* 轻微放大 */
+  transition: transform 0.3s ease;
+  /* 平滑过渡 */
 }
 
 .close-button {
@@ -448,27 +448,34 @@ html, body, #app {
 .dialog-header {
   display: flex;
   align-items: center;
-  justify-content: center; /* 将内容居中 */
+  justify-content: center;
+  /* 将内容居中 */
   width: 100%;
-  position: relative; /* 相对定位 */
+  position: relative;
+  /* 相对定位 */
 }
 
 .search-input {
-  width: 300px; /* 搜索框宽度 */
-  margin-left: 20px; /* 与标题的间距 */
+  width: 300px;
+  /* 搜索框宽度 */
+  margin-left: 20px;
+  /* 与标题的间距 */
 }
 </style>
 
 <style>
 /* 全局样式，用于固定模态框大小和内容滚动 */
 .fixed-dialog .el-dialog__body {
-  height: calc(100% - 100px); /* 减去标题和底部的高度 */
-  overflow-y: auto; /* 内容区域滚动 */
+  height: calc(100% - 100px);
+  /* 减去标题和底部的高度 */
+  overflow-y: auto;
+  /* 内容区域滚动 */
   padding: 16px;
 }
 
 .fixed-dialog .el-dialog {
-  max-height: 70vh; /* 固定高度 */
+  max-height: 70vh;
+  /* 固定高度 */
   display: flex;
   flex-direction: column;
 }
