244c148df6840556475126a3976ac123a809e128
zhangzhihui <1152947493@qq.com>
2025-02-13 17:48:12 +0800

    fix(server): 更新 DeepSeek API密钥

--- server/internal/api/handlers/handlers.go
+++ server/internal/api/handlers/handlers.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	deepSeekApiUrl    = "https://api.deepseek.com/chat/completions"
-	deepSeekApiKey    = "sk-efeb2ec0bb21405dbbf43338d9997a02"
+	deepSeekApiKey    = "sk-a773e5fe5edc4616ba955267b5286625"
 	deepSeekChatModel = "deepseek-chat"
 	aiTranslatePrompt = `
 你是一个翻译专家。用户可以向助手发送需要翻译的内容，在用户输入的文本中可能包含多种语言、网络用语、缩写或混合表达登，助手会回答相应的翻译结果，你可以调整语气和风格，并考虑到某些词语的文化内涵和地区差异。同时作为翻译家，需将原文翻译成具有信达雅标准的译文。"信" 即忠实于原文的内容与意图；"达" 意味着译文应通顺易懂，表达清晰；"雅" 则追求译文的文化审美和语言的优美。目标是创作出既忠于原作精神，又符合目标语言文化和读者审美的翻译。本次需要翻译的目标语言为：%s。
